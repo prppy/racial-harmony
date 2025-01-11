6a3e92664e6be797314532712caa6bd20bfe88de
purp <pojaroysg29@gmail.com>
2025-01-11 22:05:02 +0800

    edit

--- src/firebase.js
+++ src/firebase.js
@@ -7,7 +7,7 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDQ9hFaOKsxsg7dPL3aOqTP4GT1RAMLN30",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
   authDomain: "racialharmony-4.firebaseapp.com",
   projectId: "racialharmony-4",
   storageBucket: "racialharmony-4.firebasestorage.app",
