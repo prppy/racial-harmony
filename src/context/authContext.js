59ba8ac328d69da4cca388f013423f4ccc05f026
myathetchai <myathetchai441@gmail.com>
2025-01-12 19:01:03 +0800

    setup auth & roles with random login page

---
+++ src/context/authContext.js
@@ -0,0 +1,116 @@
+
+import { createContext, useEffect, useState, useContext } from "react";
+import { createUserWithEmailAndPassword, onAuthStateChanged, signInWithEmailAndPassword, signOut} from "firebase/auth";
+import { auth, database } from "../firebase";
+import {doc, getDoc, setDoc, collection} from "firebase/firestore"
+
+export const AuthContext = createContext();
+
+export const AuthContextProvider = ({children})  => {
+    const [user, setUser] = useState(null);
+    const [isAuthenticated, setIsAuthenticated] = useState(undefined);
+
+    // checking whether user is logged in
+    useEffect(() => {
+
+        // onAuthStateChanged
+        const unsub = onAuthStateChanged(auth, (user) => {
+            if (user) {
+                setIsAuthenticated(true)
+                setUser(user)
+                updateUserData(user.uid)
+            } else {
+                setIsAuthenticated(false)
+                setUser(null)
+            }
+
+        })
+        return unsub
+    },  [])
+
+    // creating the reference for the current User, so that in other screens, we can use {user} as a reference to the user
+    const updateUserData = async (userId) => {
+        const docRef = doc(database, "users", userId)
+        const docSnap = await getDoc(docRef)
+
+        if (docSnap.exists()) {
+            let data = docSnap.data()
+            setUser({...user, username: data.username, userId: data.userId })
+        }
+
+    }
+    // login function
+    const login = async (email, password) => {
+        try {
+            const response = await signInWithEmailAndPassword(auth, email, password)
+            return {success:true}
+
+        } catch(e) {
+            let msg = e.message
+            if (msg.includes('(auth/invalid-email)')) {
+                msg='Invalid email'
+            }
+            if (msg.includes('(auth/invalid-credential)')) {
+                msg='Wrong credentials'
+            }
+            return {success:false, msg}
+        }
+    }
+    // logout function
+    const logout = async () => {
+        try {
+
+            await signOut(auth)
+            return {success:true}
+
+        } catch(e) {
+            return {success: false, msg: e.message, error:e}
+            
+        }
+    }
+    // register function
+    const register = async (email, password, name, admin) => {
+        try {
+            const response = await createUserWithEmailAndPassword(auth, email, password)
+            console.log("response.user: ", response?.user)
+
+            await setDoc(doc(database, "users", response?.user?.uid), {
+                name,
+                email,
+                userId: response?.user?.uid,
+                admin: admin
+            } )
+            return {success:true, data: response?.user}
+
+            
+        } catch(e) {
+            let msg = e.message
+            if (msg.includes('(auth/invalid-email)')) {
+                msg='Invalid email'
+            }
+            if (msg.includes('(auth/email-already-in-use)')) {
+                msg = "This email is already in use"
+            }
+            return {success:false, msg}
+            
+        }
+    }
+
+    return (
+        <AuthContext.Provider value = {{user, isAuthenticated, login, register, logout}}>
+            {children}
+        </AuthContext.Provider>
+    )
+
+}
+
+export const useAuth = () => {
+    const value = useContext(AuthContext)
+
+    if (!value) {
+        throw new Error('useAuth must be wrapped inside AuthContextProvider')
+    }
+
+    return value
+}
+
