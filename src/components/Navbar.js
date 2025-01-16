7c5abd273f78d86dff41f3fff2a507226a0f1213
purp <pojaroysg29@gmail.com>
2025-01-17 04:00:47 +0800

    navbar ui updated

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,147 +1,179 @@
-import React from 'react';
-import { NavLink } from 'react-router-dom';
-import { RED } from '../constants/colors';
-const Navbar = ({admin}) => {
-  
+import React from "react";
+import { NavLink } from "react-router-dom";
+import { RED, DARK_PURPLE } from "../constants/colors";
+const Navbar = ({ admin }) => {
+    const isAdmin = admin;
+    return (
+        <div style={navbarStyles.navbar}>
+            <NavLink to="/">
+                <img src="/logo.png" alt="Logo" style={navbarStyles.logo} />
+            </NavLink>
+            <div style={navbarStyles.tabs}>
+                {!isAdmin ? (
+                    <>
+                        <NavLink
+                            to="/minimart"
+                            style={({ isActive }) =>
+                                isActive
+                                    ? {
+                                          ...navbarStyles.tab,
+                                          color: RED,
+                                          borderBottom: `2px solid ${RED}`,
+                                      }
+                                    : navbarStyles.tab
+                            }
+                        >
+                            Minimart
+                        </NavLink>
 
-  const isAdmin = admin;
-  return (
-    <div style={navbarStyles.navbar}>
-      <NavLink to="/">
-        <img src="/logo.png" alt="Logo" style={navbarStyles.logo} />
-      </NavLink>
-      <div style={navbarStyles.tabs}>
+                        <NavLink
+                            to="/vouchers"
+                            style={({ isActive }) =>
+                                isActive
+                                    ? {
+                                          ...navbarStyles.tab,
+                                          color: RED,
+                                          borderBottom: `2px solid ${RED}`,
+                                      }
+                                    : navbarStyles.tab
+                            }
+                        >
+                            Vouchers
+                        </NavLink>
+                        <NavLink
+                            to="/leaderboard"
+                            style={({ isActive }) =>
+                                isActive
+                                    ? {
+                                          ...navbarStyles.tab,
+                                          color: RED,
+                                          borderBottom: `2px solid ${RED}`,
+                                      }
+                                    : navbarStyles.tab
+                            }
+                        >
+                            Leaderboard
+                        </NavLink>
+                        <NavLink
+                            to="/history"
+                            style={({ isActive }) =>
+                                isActive
+                                    ? {
+                                          ...navbarStyles.tab,
+                                          color: RED,
+                                          borderBottom: `2px solid ${RED}`,
+                                      }
+                                    : navbarStyles.tab
+                            }
+                        >
+                            History
+                        </NavLink>
+                    </>
+                ) : (
+                    <>
+                        <NavLink
+                            to="/manage"
+                            style={({ isActive }) =>
+                                isActive
+                                    ? {
+                                          ...navbarStyles.tab,
+                                          color: RED,
+                                          borderBottom: `2px solid ${RED}`,
+                                      }
+                                    : navbarStyles.tab
+                            }
+                        >
+                            Manage
+                        </NavLink>
+                        <NavLink
+                            to="/reports"
+                            style={({ isActive }) =>
+                                isActive
+                                    ? {
+                                          ...navbarStyles.tab,
+                                          color: RED,
+                                          borderBottom: `2px solid ${RED}`,
+                                      }
+                                    : navbarStyles.tab
+                            }
+                        >
+                            Reports
+                        </NavLink>
 
-        {!isAdmin? <>
-          <NavLink
-          to="/minimart"
-          style={({ isActive }) =>
-            isActive
-              ? { ...navbarStyles.tab, color: RED, borderBottom:  `3px solid ${RED}` }
-              : navbarStyles.tab
-          }
-        >
-          Minimart
-        </NavLink>
-        <NavLink
-          to="/leaderboard"
-          style={({ isActive }) =>
-            isActive
-              ? { ...navbarStyles.tab, color: RED, borderBottom:  `3px solid ${RED}` }
-              : navbarStyles.tab
-          }
-        >
-          Leaderboard
-        </NavLink>
-        
-        <NavLink
-          to="/vouchers"
-          style={({ isActive }) =>
-            isActive
-              ? { ...navbarStyles.tab, color: RED, borderBottom:  `3px solid ${RED}` }
-              : navbarStyles.tab
-          }
-        >
-          Vouchers
-        </NavLink>
-        <NavLink
-          to="/history"
-          style={({ isActive }) =>
-            isActive
-              ? { ...navbarStyles.tab, color: RED, borderBottom:  `3px solid ${RED}` }
-              : navbarStyles.tab
-          }
-        >
-          History
-        </NavLink>
-        
-        </> : <>
-        
-        <NavLink
-          to="/manage"
-          style={({ isActive }) =>
-            isActive
-              ? { ...navbarStyles.tab, color: RED, borderBottom:  `3px solid ${RED}` }
-              : navbarStyles.tab
-          }
-        >
-          Manage
-        </NavLink>
-        <NavLink
-          to="/reports"
-          style={({ isActive }) =>
-            isActive
-              ? { ...navbarStyles.tab, color: RED, borderBottom:  `3px solid ${RED}` }
-              : navbarStyles.tab
-          }
-        >
-          Reports
-        </NavLink>
-        
-        <NavLink
-          to="/requests"
-          style={({ isActive }) =>
-            isActive
-              ? { ...navbarStyles.tab, color: RED, borderBottom:  `3px solid ${RED}` }
-              : navbarStyles.tab
-          }
-        >
-          Requests
-        </NavLink>
-        <NavLink
-          to="/tasks"
-          style={({ isActive }) =>
-            isActive
-              ? { ...navbarStyles.tab, color: RED, borderBottom:  `3px solid ${RED}` }
-              : navbarStyles.tab
-          }
-        >
-          Tasks
-        </NavLink>
-        </>
-        
-    
-      }
-        
-        <NavLink
-          to="/profile"
-          style={({ isActive }) =>
-            isActive
-              ? { ...navbarStyles.tab, color: RED, borderBottom:  `3px solid ${RED}` }
-              : navbarStyles.tab
-          }
-        >
-          Profile
-        </NavLink>
-      </div>
-    </div>
-  );
+                        <NavLink
+                            to="/requests"
+                            style={({ isActive }) =>
+                                isActive
+                                    ? {
+                                          ...navbarStyles.tab,
+                                          color: RED,
+                                          borderBottom: `2px solid ${RED}`,
+                                      }
+                                    : navbarStyles.tab
+                            }
+                        >
+                            Requests
+                        </NavLink>
+                        <NavLink
+                            to="/tasks"
+                            style={({ isActive }) =>
+                                isActive
+                                    ? {
+                                          ...navbarStyles.tab,
+                                          color: RED,
+                                          borderBottom: `2px solid ${RED}`,
+                                      }
+                                    : navbarStyles.tab
+                            }
+                        >
+                            Tasks
+                        </NavLink>
+                    </>
+                )}
+
+                <NavLink
+                    to="/profile"
+                    style={({ isActive }) =>
+                        isActive
+                            ? {
+                                  ...navbarStyles.tab,
+                                  color: RED,
+                                  borderBottom: `2px solid ${RED}`,
+                              }
+                            : navbarStyles.tab
+                    }
+                >
+                    Profile
+                </NavLink>
+            </div>
+        </div>
+    );
 };
 
 const navbarStyles = {
-  navbar: {
-    display: 'flex',
-    justifyContent: 'space-between',
-    alignItems: 'center',
-    backgroundColor: '#FFFFFF',
-    padding: '10px 20px',
-    color: 'white',
-  },
-  logo: {
-    height: '60px',
-    cursor: 'pointer',
-  },
-  tabs: {
-    display: 'flex',
-    gap: '20px',
-  },
-  tab: {
-    color: 'black',
-    textDecoration: 'none',
-    fontWeight: 'bold',
-    paddingBottom: '5px', // Space between text and underline
-
-  },
+    navbar: {
+        display: "flex",
+        justifyContent: "space-between",
+        alignItems: "center",
+        backgroundColor: "#FFFFFF",
+        paddingRight: "50px",
+        color: "white",
+    },
+    logo: {
+        height: "100px",
+        cursor: "pointer",
+    },
+    tabs: {
+        display: "flex",
+        gap: "25px",
+        fontSize: "16px",
+    },
+    tab: {
+        color: { DARK_PURPLE },
+        textDecoration: "none",
+        fontWeight: "bold",
+        paddingBottom: "5px", // Space between text and underline
+    },
 };
 
 export default Navbar;
